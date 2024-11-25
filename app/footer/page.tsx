9cffed9aa040704c038f0e7b497b9bb9008242e1
Jherico <jhericof.sv.189@gmail.com>
2024-11-24 21:48:11 -0500

    add background video in home

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -1,7 +1,17 @@
 const Footer = () => (
   <footer className="bg-gray-900 text-white py-6">
     <div className="text-center space-y-4">
-      <p>&copy; 2024 <a href="https://www.jhericosolier.com" target="_blank">Jherico Solier</a>. Todos los derechos reservados.</p>
+      <p>
+        &copy; 2024{" "}
+        <a
+          href="https://www.jhericosolier.com"
+          target="_blank"
+          className="font-bold hover:text-blue-400"
+        >
+          Jherico Solier
+        </a>
+        . Todos los derechos reservados.
+      </p>
       <div className="flex justify-center space-x-6">
         <a href="#" className="hover:text-blue-400">
           Facebook
--- app/sections/home/page.tsx
+++ app/sections/home/page.tsx
@@ -1,15 +1,28 @@
 const Home = () => (
-  <section id="home" className="-mt-16 flex flex-col md:flex-row items-center justify-center md:justify-between p-8 bg-gray-400 text-white h-screen">
-    <div className="max-w-lg lg:max-w-xl xl:max-w-2xl text-center md:text-left">
-      <h1 className="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold mb-4">
-        Descubre el futuro con ProPhone
+  <section
+    id="home"
+    className="-mt-16 flex flex-col md:flex-row items-center justify-center md:justify-between p-8 lg:pl-24 xl:pl-36 2xl:pl-48 bg-gray-400 text-white h-screen relative overflow-hidden"
+  >
+    <video
+      autoPlay
+      loop
+      muted
+      className="absolute top-0 left-0 w-full h-full object-cover z-0"
+    >
+      <source src="/videos/video_home.webm" type="video/webm" />
+    </video>
+    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-35 z-5"></div>
+    <div className="relative z-10 max-w-lg lg:max-w-xl xl:max-w-5xl text-center md:text-left">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold mb-4 font-serif text-shadow">
+        Descubre el futuro con{" "}
+        <span className="animate-colors font-black">ProPhone</span>
       </h1>
-      <p className="text-gray-700 mb-6">
-        El teléfono inteligente diseñado para simplificar tu vida.
+      <p className="mb-6 text-shadow 2xl:text-lg">
+        El teléfono diseñado para simplificar tu vida.
       </p>
       <a
         href="#"
-        className="bg-blue-500 px-6 py-3 rounded-lg hover:bg-blue-600"
+        className="bg-blue-500 px-6 py-3 rounded-lg hover:bg-blue-600 shadow-sm hover:shadow-md shadow-zinc-700 hover:shadow-zinc-700"
       >
         ¡Reserva el tuyo ahora!
       </a>
